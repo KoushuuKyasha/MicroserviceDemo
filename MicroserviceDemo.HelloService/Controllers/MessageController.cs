7ce1ab1ca969b25eb8b09f5998953225c5b786ec
Henry Chu <henrytech@outlook.com>
2018-06-30 20:33:27 +0800

    added hello service

---
+++ MicroserviceDemo.HelloService/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MicroserviceDemo.HelloService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet("")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        public ActionResult<string> Index()
+        {
+            return $"Hello from .NET Core Service at {DateTime.Now}";
+        }
+    }
+}
